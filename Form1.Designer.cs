cd71d59e144f307bcb3b71cd44b34c43213ec9f6
Božidar Tovarnicki <121364880+Dovvahkiin@users.noreply.github.com>
2024-10-31 14:24:47 +0100

    Version 1.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace TypicalYasuo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbTeemo = new System.Windows.Forms.PictureBox();
+            this.pbYasuo = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pbTornado = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpet = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTeemo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbYasuo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTornado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbTeemo
+            // 
+            this.pbTeemo.BackColor = System.Drawing.Color.Transparent;
+            this.pbTeemo.Enabled = false;
+            this.pbTeemo.Image = global::TypicalYasuo.Properties.Resources.teemo1;
+            this.pbTeemo.Location = new System.Drawing.Point(336, 459);
+            this.pbTeemo.Name = "pbTeemo";
+            this.pbTeemo.Size = new System.Drawing.Size(97, 105);
+            this.pbTeemo.TabIndex = 0;
+            this.pbTeemo.TabStop = false;
+            // 
+            // pbYasuo
+            // 
+            this.pbYasuo.BackColor = System.Drawing.Color.Transparent;
+            this.pbYasuo.Enabled = false;
+            this.pbYasuo.Image = global::TypicalYasuo.Properties.Resources.yas_1;
+            this.pbYasuo.Location = new System.Drawing.Point(336, 0);
+            this.pbYasuo.Name = "pbYasuo";
+            this.pbYasuo.Size = new System.Drawing.Size(97, 105);
+            this.pbYasuo.TabIndex = 1;
+            this.pbYasuo.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pbTornado
+            // 
+            this.pbTornado.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbTornado.Image = global::TypicalYasuo.Properties.Resources.tornado_1;
+            this.pbTornado.Location = new System.Drawing.Point(336, 111);
+            this.pbTornado.Name = "pbTornado";
+            this.pbTornado.Size = new System.Drawing.Size(101, 109);
+            this.pbTornado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbTornado.TabIndex = 2;
+            this.pbTornado.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // btnOpet
+            // 
+            this.btnOpet.Location = new System.Drawing.Point(12, 48);
+            this.btnOpet.Name = "btnOpet";
+            this.btnOpet.Size = new System.Drawing.Size(116, 39);
+            this.btnOpet.TabIndex = 5;
+            this.btnOpet.Text = "Pokušaj ponovo";
+            this.btnOpet.UseVisualStyleBackColor = true;
+            this.btnOpet.Click += new System.EventHandler(this.btnOpet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TypicalYasuo.Properties.Resources.teren;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnOpet);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbTornado);
+            this.Controls.Add(this.pbYasuo);
+            this.Controls.Add(this.pbTeemo);
+            this.KeyPreview = true;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Typical Yasuo Tornadoes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbTeemo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbYasuo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTornado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbTeemo;
+        private System.Windows.Forms.PictureBox pbYasuo;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pbTornado;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpet;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TypicalYasuo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random r = new Random();
+        int xLock;
+        int yLock;
+        int tornadoY; 
+        int tornadoX; 
+        int a;
+        int b;
+        int vreme;
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) // u zagradi je defenisano "e" koji služi za tastere
+        {
+            int x = pbTeemo.Location.X; // lokacija slike (picture boxa) teema X osa
+            int y = pbTeemo.Location.Y; // lokacija slike (picture boxa) teema Y osa
+
+
+            if ((e.KeyCode == Keys.Right) || (e.KeyCode == Keys.D)) x += 10; // pomeranje u desno
+            else if ((e.KeyCode == Keys.Left) || (e.KeyCode == Keys.A)) x -= 12; // pomeranje u levo
+            pbTeemo.Location = new Point(x, y); // ucitavanje lokacije teema
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            xLock = this.Width;
+            yLock = this.Height;
+            a = r.Next(10, 20);
+            b = 0;
+            tornadoY = 0;
+            vreme = 0;
+            label1.Text = "Ukupno preživelih sekundi: 0"; // label sa rezultatom
+            pomeranjeSlike();
+            pbTornado.Visible = false;
+            btnOpet.Visible = false;
+
+        }
+
+        private void pomeranjeSlike() // pomeranje slike yasua
+        {
+            int x = pbYasuo.Location.X;
+            int y = pbYasuo.Location.Y;
+
+            x = r.Next (150, x+= 150 ); // next vraća random broj u range od minimuma i maximuma (150, x+=150)
+
+            pbYasuo.Location = new Point(x, y);
+        }
+        private void timer1_Tick(object sender, EventArgs e) // Yasuovo kretanje uz funkciju
+        {
+            pomeranjeSlike(); // pozivanje funkcije za pomeranje slike
+        }
+        private void timer2_Tick(object sender, EventArgs e) // kretanje tornada
+        {
+            int x = pbTeemo.Location.X;
+            b++; // Svaki put kad timer odradi tick b se poveća za 1, a vrednost a koja je random je određena na otvaranje forme
+            vreme++; // tajmer
+            if (vreme % 10 == 0)
+                label1.Text = " Ukupno preživelih sekundi:" + vreme / 10;
+            if(b == a) // ako se vrednost b i random vrednost a poklope pojavljuje se tornado sa x pozicije yasua
+            {
+                pbTornado.Visible = true;
+                tornadoX = pbYasuo.Location.X;
+            }
+            else if (b > a) // kada se promeni vrednost tornado ide vertikalno
+            {
+                tornadoY += 35;
+                pbTornado.Location = new Point(tornadoX, tornadoY);
+                if (tornadoY > Height) // kada tornado nestane sa forma dobija početnu vrednost
+                {
+                    b = 0; // vrednost b se postavlja na 0 jer sledeći tick na timer dodaje vrednost tako da će biti ponovo 1,2,3...
+                    tornadoY = 0;
+                    a = r.Next(10, 20); // vrednost a dobije ponovo neku drugu random vrednost 
+                }
+            }
+            if (pbTornado.Bounds.IntersectsWith(pbTeemo.Bounds)) // ako se teemo i tornado poklope poziva se funkcija gameover
+            {
+                gameover(); 
+            }
+        }
+        private void gameover() // funkcija za kraj igre
+        {
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            btnOpet.Visible = true;
+
+        }
+
+        private void btnOpet_Click(object sender, EventArgs e) // dugme za ponovni pokušaj sa svim vrednostima jednakim nuli
+        {
+            vreme = 0;
+            a = 0;
+            b = 0;
+            tornadoX = 0;
+            tornadoY = 0;
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            btnOpet.Visible = false;
+            label1.Text = "Ukupno preživelih sekundi: 0";
+        }
+
+
+
+
+
+
+    }
+}
